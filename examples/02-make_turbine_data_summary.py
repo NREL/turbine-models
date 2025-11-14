ea889df5b1dfd220cab94578ed45cd2460c0666b
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-11-14 10:05:54 -0700

    updated example 2

--- examples/02-make_turbine_data_summary.py
+++ examples/02-make_turbine_data_summary.py
@@ -1,7 +1,7 @@
 from turbine_models.make_turbine_data_summary import create_turbine_data_summary
 import os
 
-output_dir = os.path.dirname(__file__,"outputs")
+output_dir = os.path.join(os.path.dirname(__file__),"outputs")
 if not os.path.isdir(output_dir):
     os.makedirs(output_dir)
 
