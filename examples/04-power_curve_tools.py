dca91f6520f714fe13160708a6fe55e67ffe253c
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-11-21 15:17:32 -0700

    added power-curve plotting to example 4

--- examples/04-power_curve_tools.py
+++ examples/04-power_curve_tools.py
@@ -1,12 +1,16 @@
 from turbine_models.parser import Turbines
 from turbine_models.tools.extract_power_curve import extract_power_curve
+from turbine_models.tools.power_curve_tools import plot_power_curve
 from turbine_models.tools.library_tools import check_turbine_library_for_turbine
 
 turbine = "GE_1.5MW"
 t_lib = Turbines()
 
+is_valid = check_turbine_library_for_turbine(turbine)
+
 turb_group = t_lib.find_group_for_turbine(turbine)
 turbine_specs = t_lib.specs(turbine,group = turb_group)
 
-is_valid = check_turbine_library_for_turbine(turbine)
 power_curve = extract_power_curve(turbine_specs)
+
+plot_power_curve(power_curve["wind_speed"],power_curve["cp_curve"],power_curve["ct_curve"])
